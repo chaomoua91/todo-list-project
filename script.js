579af8adcbc8a2e7af04b774efda80929354498e
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-07-27 15:05:13 -0500

    updated styling and JS

--- script.js
+++ script.js
@@ -1,57 +1,157 @@
-const todoList = [{
-   name: 'make dinner',
-   dueDate: '04-06-2024'},
-   { 
-   name:'wash dishes',
-   dueDate: '04-06-2024',}
-   ];
+const listsContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector("[data-new-list-form]");
+const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("[data-delete-list-button]");
+const listDisplayContainer = document.querySelector(
+  "[data-list-display-container]"
+);
+const listTitleElement = document.querySelector("[data-list-title]");
+const listCountElement = document.querySelector("[data-list-count]");
+const tasksContainer = document.querySelector("[data-tasks]");
+const taskTemplate = document.getElementById("task-template");
+const newTaskForm = document.querySelector("[data-new-task-form]");
+const newTaskInput = document.querySelector("[data-new-task-input]");
+const clearCompleteTaskButton = document.querySelector(
+  "[data-clear-complete-tasks-button]"
+);
 
-renderTodoList();
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
-function renderTodoList () {
-   let todoListHTML = '';
+listsContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
 
-   for (let i= 0; i < todoList.length; i++) {
-      const todoObject = todoList[i];
-      //const name = todoObject.name;
-      //const dueDate = todoObject.dueDate;
-      const { name, dueDate } = todoObject;
-      const html = `
-      <div>${name}</div>
-      <div>${dueDate}</div>
-      <button onclick="
-         todoList.splice(${i}, 1);
-         renderTodoList();
-      " class="delete-todo-button">Delete</button>
-      
-      `;
-      todoListHTML += html;
-   }
+tasksContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "input") {
+    const selectedList = lists.find((list) => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find(
+      (task) => task.id === e.target.id
+    );
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
+  }
+});
 
+clearCompleteTaskButton.addEventListener("click", (e) => {
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks = selectedList.tasks.filter((task) => !task.complete);
+  saveAndRender();
+});
 
-   document.querySelector('.js-todo-list')
-      .innerHTML = todoListHTML;
+deleteListButton.addEventListener("click", (e) => {
+  lists = lists.filter((list) => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
+});
+
+newListForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === "") return;
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+});
+
+newTaskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === "") return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
+
+function createList(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: [],
+  };
 }
 
+function createTask(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    complete: false,
+  };
+}
 
-function addTodo() {
-   const inputElement = document.querySelector('.js-name-input');
-   const name = inputElement.value;
+function saveAndRender() {
+  save();
+  render();
+}
 
-   const dateInputElement = document.querySelector('.js-due-date-input');
-   const dueDate = dateInputElement.value;
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
 
+function render() {
+  clearElement(listsContainer);
+  renderLists();
 
-   todoList.push({
-      //name: name,
-      //dueDate: dueDate
-      name,
-      dueDate
-   });
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = "none";
+  } else {
+    listDisplayContainer.style.display = "";
+    listTitleElement.innerText = selectedList.name;
+    renderTaskCount(selectedList);
+    clearElement(tasksContainer);
+    renderTasks(selectedList);
+  }
+}
 
+function renderTasks(selectedList) {
+  selectedList.tasks.forEach((task) => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkbox = taskElement.querySelector("input");
+    checkbox.id = task.id;
+    checkbox.checked = task.complete;
+    const label = taskElement.querySelector("label");
+    label.htmlFor = task.id;
+    label.append(task.name);
+    tasksContainer.appendChild(taskElement);
+  });
+}
 
-   inputElement.value = '';
+function renderTaskCount(selectedList) {
+  const incompleteTasksCount = selectedList.tasks.filter(
+    (task) => !task.complete
+  ).length;
+  const taskString = incompleteTasksCount === 1 ? "task" : "tasks";
+  listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining`;
+}
 
+function renderLists() {
+  lists.forEach((list) => {
+    const listElement = document.createElement("li");
+    listElement.dataset.listId = list.id;
+    listElement.classList.add("list-name");
+    listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add("active-list");
+    }
+    listsContainer.appendChild(listElement);
+  });
+}
 
-   renderTodoList();
-} 
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+render();
